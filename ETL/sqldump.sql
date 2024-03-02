9b23b9877b1f33dd0e55a9be5b49812f27e12ac3
Colton Gagel <coltongagel@gmail.com>
2024-03-02 11:39:22 -0800

    Adding in watchlists to backend

--- ETL/sqldump.sql
+++ ETL/sqldump.sql
@@ -65,8 +65,58 @@ LEFT JOIN maxdates ON s.symbol = maxdates.symbol
 
 
 
+SELECT sq.symbol, sq."date", sq."open", sq.high, sq.low, sq."close", sq.adj_close, sq.volume
+FROM dbo.symbol_quotes sq
+join dbo.symbols s on s.symbol = sq.symbol 
+	and s.active = '1'
+	
+	
+SELECT * FROM dbo.symbols 
 
 
 
 
+	
+	SELECT symbol, "type", subtype, exchange, category, description, "comment", active
+	FROM dbo.symbols where symbol = 'MDT'
 
+-- delete from dbo.symbols where symbol = 'MDT'
+
+-- update dbo.symbols set type = 'STOCK' where symbol in ('ILMN','BABA')
+	
+SELECT * FROM dbo.watchlists 
+
+
+-- drop table dbo.watchlists 
+create table dbo.watchlists (
+	id serial not null primary key,
+	shortname varchar unique not null, 
+	longname varchar
+	)
+
+insert into dbo.watchlists (shortname, longname) select 'Algo','Trading algorithm potentials'
+
+
+select * from dbo.watchlists 
+
+
+-- drop table dbo.watchlistsymbols
+create table dbo.watchlistsymbols (
+	id serial not null, 
+	watchlist_id int REFERENCES dbo.watchlists(id), 
+	symbol varchar REFERENCES dbo.symbols(symbol),
+	primary key (watchlist_id,symbol)
+	)
+
+
+-- create default watchlist 
+insert into dbo.watchlistsymbols (watchlist_id, symbol)
+	select (select id from dbo.watchlists where shortname = 'Algo'), symbol 
+	from dbo.symbols 
+	where symbol in ('OMIC','ALGN')
+
+
+	
+	
+	
+	
